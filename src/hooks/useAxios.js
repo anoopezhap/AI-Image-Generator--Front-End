68614ff88bd115e4a1b2163be59b9e87a5ea0b9c
anoopezhap <anoopezhap@gmail.com>
2024-04-19 10:48:44 -0400

    done

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 export default function useAxios() {
   const axios = Axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://anoopsai-api.onrender.com",
   });
 
   return axios;
